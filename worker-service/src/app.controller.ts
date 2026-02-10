832f7ea6790111fc32038894db25e4b3e708db80
atulbhartiproff <atulbhartiproff@gmail.com>
2026-02-11 00:48:54 +0530

    Added Database and db controller

--- worker-service/src/app.controller.ts
+++ worker-service/src/app.controller.ts
@@ -1,24 +1,70 @@
 import { Controller, Get } from '@nestjs/common';
 import { EventPattern, Payload } from '@nestjs/microservices';
 import {redis} from './redis.client';
+import { db } from './db.client';
+
+
+
+
+
+
+// @Controller()
+// export class AppController {
+//   @EventPattern('event_created')
+//   async handleEvent(@Payload() data:any){
+//     const eventId=data.event_id;
+
+//     const alreadyProcessed=await redis.get('processed:${eventId}');
+
+//     if(alreadyProcessed){
+//       console.log('Event already processed, skipping:', eventId);
+//       return;
+//     }
+
+//     console.log('Processing event:', eventId);
+
+//     await redis.set('processed:${eventId}', 'true');
+
+//     await db.query(
+//       `INSERT INTO events (event_id, type, timestamp, payload)
+//    VALUES ($1, $2, $3, $4)`,
+//   [eventId, data.type, data.timestamp, data.payload],
+//     );
+
+//     console.log(`🗄 Event stored in Postgres: ${eventId}`);
+//     console.log('Event processed and marked as done:', eventId);
+//   }
+// }
+
 
 @Controller()
 export class AppController {
   @EventPattern('event_created')
-  async handleEvent(@Payload() data:any){
-    const eventId=data.event_id;
+  async handleEvent(@Payload() data: any) {
+    const eventId = data.event_id;
 
-    const alreadyProcessed=await redis.get('processed:${eventId}');
+    // 1. Idempotency check
+    const alreadyProcessed = await redis.get(`processed:${eventId}`);
 
-    if(alreadyProcessed){
-      console.log('Event already processed, skipping:', eventId);
+    if (alreadyProcessed) {
+      console.log('Duplicate skipped:', eventId);
       return;
     }
 
     console.log('Processing event:', eventId);
 
-    await redis.set('processed:${eventId}', 'true');
+    // 2. Store in Postgres
+    await db.query(
+      `INSERT INTO events (event_id, type, timestamp, payload)
+       VALUES ($1, $2, $3, $4)`,
+      [eventId, data.type, data.timestamp, data.payload],
+    );
 
-    console.log('Event processed and marked as done:', eventId);
+    console.log(`Stored in Postgres: ${eventId}`);
+
+    // 3. Mark processed only after DB success
+    await redis.set(`processed:${eventId}`, 'true');
+
+    console.log('Event marked done:', eventId);
   }
 }
